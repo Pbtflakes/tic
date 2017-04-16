177ac53187063f467d5fd31ad8c2db559248e65e
Matthew Lavin <pbtflakes@protonmail.ch>
2017-04-16 15:48:58 -0400

    stuff

---
+++ tic.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+enum {
+	S = 3
+};
+
+/* Type for board locations, placed pieces are A or B while empty spot is N */
+enum game_t {
+	N,
+	A,
+	B
+};
+
+void	boardstate(game_t[][S]);
+game_t	winstate(game_t[][S]);
+game_t	winrow(game_t[][S]);
+
+int
+main(void) {
+	game_t board[S][S];
+	for (int i = 0; i < S; i++) {
+		for (int j = 0; j < S; j++) {
+			board[i][j] = N;
+		}
+	}
+
+	boardstate(board);
+
+	return 0;
+}
+
+/* Prints out the board's current state. All empty squares are 0, squares with
+ * piece from player A are 1, squares with piece from player B are 2. A bit of
+ * polishing would be to output as 'X' and 'O' if I've the time. */
+void
+boardstate(game_t state[][S]) {
+	for (int i = 0; i < S; i++) {
+		for (int j = 0; j < S; j++) {
+			std::cout << state[i][j];
+		}
+		std::cout << std::endl;
+	}
+}
+
+/* Check whether the inputted board state satisfies the win condition for
+ * either player. This is probably going to be the most ugly bit of code;
+ * specific types of winstate may be spun into their own subfunctions. */
+game_t
+winstate(game_t state[][S]) {
+	if (winrow(state) != N) {
+		return winrow(state);
+	}
+	return N;
+}
+
+game_t
+winrow(game_t state[][S]) {
+	for (int i = 0; i < S; i++) {
+		if (state[i][0] == state[i][1] && state[i][1] == state[i][2])
+			return state[i][0];
+	}
+	return N;
+}
